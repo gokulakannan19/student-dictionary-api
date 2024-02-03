2ec53b8367519df7ccb1ec54ed5c15d8cfd38109
gokulakannan <gokulakannan.r@idp.com>
2024-02-03 11:46:53 +0530

    dictionary built using flask

---
+++ app.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from flask import Flask, render_template
+
+app = Flask("__name__")
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/api/v1/<word>")
+def api(word):
+    df = pd.read_csv("dictionary.csv")
+    definition = df.loc[df["word"] == word]["definition"].squeeze()
+    print(definition)
+    print("*********************************************************************************")
+    result_dictionary = {"word": word, "definition": definition}
+    return result_dictionary
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5002)
